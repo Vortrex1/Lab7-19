04a53ea7e325439e2fe81023d7ddba1eae5171b3
Vortrex1 <145112973+Vortrex1@users.noreply.github.com>
2024-05-27 17:41:28 +0300

    feat: Lab-14

--- Hladunenko_lavarel/blog/routes/web.php
+++ Hladunenko_lavarel/blog/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RestTestController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,3 +16,23 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+Route::resource('rest', RestTestController::class)->names('restTest');
+Route::group([ 'namespace' => 'App\Http\Controllers\Blog', 'prefix' => 'blog'], function () {
+    Route::resource('posts', PostController::class)->names('blog.posts');
+});
+//Адмінка
+$groupData = [
+    'namespace' => 'App\Http\Controllers\Blog\Admin',
+    'prefix' => 'admin/blog',
+];
+Route::group($groupData, function () {
+    //BlogCategory
+    $methods = ['index','edit','store','update','create',];
+    Route::resource('categories', CategoryController::class)
+    ->only($methods)
+    ->names('blog.admin.categories'); 
+    Route::resource('posts', PostController::class)
+    ->except(['show'])                               //не робити маршрут для метода show
+    ->names('blog.admin.posts');
+ });
+ 
