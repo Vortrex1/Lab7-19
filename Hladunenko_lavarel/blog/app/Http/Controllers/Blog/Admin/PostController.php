ce32d8742d5a080ccac5e6cf8eed6900c74679ef
Vortrex1 <145112973+Vortrex1@users.noreply.github.com>
2024-05-27 18:21:28 +0300

    feat: lab 17

--- Hladunenko_lavarel/blog/app/Http/Controllers/Blog/Admin/PostController.php
+++ Hladunenko_lavarel/blog/app/Http/Controllers/Blog/Admin/PostController.php
@@ -3,7 +3,12 @@
 namespace App\Http\Controllers\Blog\Admin;
 
 use App\Repositories\BlogPostRepository;
+use App\Repositories\BlogCategoryRepository;
+use App\Http\Requests\BlogPostUpdateRequest;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use App\Models\BlogPost;
+use App\Http\Requests\BlogPostCreateRequest;
 
 class PostController extends BaseController
 {
@@ -11,11 +16,12 @@ class PostController extends BaseController
      * @var BlogPostRepository
      */
     private $blogPostRepository;
-
+    private $blogCategoryRepository;
     public function __construct()
     {
         parent::__construct();
         $this->blogPostRepository = app(BlogPostRepository::class); //app вертає об'єкт класа
+        $this->blogCategoryRepository = app(BlogCategoryRepository::class);
     }
     /**
      * Display a listing of the resource.
@@ -32,15 +38,31 @@ class PostController extends BaseController
      */
     public function create()
     {
-        //
+        $item = new BlogPost();
+        $categoryList = $this->blogCategoryRepository->getForComboBox();
+        
+
+        return view('blog.admin.posts.edit', compact('item', 'categoryList'));
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(BlogPostCreateRequest $request)
     {
-        //
+        $data = $request->input(); //отримаємо масив даних, які надійшли з форми
+
+        $item = (new BlogPost())->create($data); //створюємо об'єкт і додаємо в БД
+
+        if ($item) {
+            return redirect()
+                ->route('blog.admin.posts.edit', [$item->id])
+                ->with(['success' => 'Успішно збережено']);
+        } else {
+            return back()
+                ->withErrors(['msg' => 'Помилка збереження'])
+                ->withInput();
+        }
     }
 
     /**
@@ -56,7 +78,13 @@ class PostController extends BaseController
      */
     public function edit(string $id)
     {
-        //
+        $item = $this->blogPostRepository->getEdit($id);
+        if (empty($item)) {                         //помилка, якщо репозиторій не знайде наш ід
+            abort(404);
+        }
+        $categoryList = $this->blogCategoryRepository->getForComboBox();
+     
+        return view('blog.admin.posts.edit', compact('item', 'categoryList'));
     }
 
     /**
@@ -64,7 +92,27 @@ class PostController extends BaseController
      */
     public function update(Request $request, string $id)
     {
-        //
+        $item = $this->blogPostRepository->getEdit($id);
+        if (empty($item)) { //якщо ід не знайдено
+            return back() //redirect back
+                ->withErrors(['msg' => "Запис id=[{$id}] не знайдено"]) //видати помилку
+                ->withInput(); //повернути дані
+        }
+
+        $data = $request->all(); //отримаємо масив даних, які надійшли з форми
+        
+          
+        $result = $item->update($data); //оновлюємо дані об'єкта і зберігаємо в БД
+
+        if ($result) {
+            return redirect()
+                ->route('blog.admin.posts.edit', $item->id)
+                ->with(['success' => 'Успішно збережено']);
+        } else {
+            return back()
+                ->with(['msg' => 'Помилка збереження'])
+                ->withInput();
+        }
     }
 
     /**
@@ -72,6 +120,17 @@ class PostController extends BaseController
      */
     public function destroy(string $id)
     {
-        //
+        $result = BlogPost::destroy($id); //софт деліт, запис лишається
+
+        //$result = BlogPost::find($id)->forceDelete(); //повне видалення з БД
+
+        if ($result) {
+            return redirect()
+                ->route('blog.admin.posts.index')
+                ->with(['success' => "Запис id[$id] видалено"]);
+        } else {
+            return back()
+                ->withErrors(['msg' => 'Помилка видалення']);
+        }
     }
 }
